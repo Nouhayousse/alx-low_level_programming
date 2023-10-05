4a5db3b71909e61bbbb526bd56bda6d7571c0084
Nouhayousse <nouhaila.youssefi@um5r.ac.ma>
2023-10-05 14:13:06 -0700

    C hello commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#!bin/bash
+#include<stdio.h>
+#include<stdlib.h>
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
