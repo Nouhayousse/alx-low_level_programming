efcb8efca4f3b121a7965d94a4b34190cc20516f
Nouhayousse <nouhaila.youssefi@um5r.ac.ma>
2023-10-05 16:57:06 -0700

    C hello commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+  * main - A program that prints a link using puts
+  *
+  *Description: prints programming is like building a multilingual
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
