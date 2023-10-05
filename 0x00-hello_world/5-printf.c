28bb9b8a0b0e73d5b9cf1ecc298d95a39c58aaa8
Nouhayousse <nouhaila.youssefi@um5r.ac.ma>
2023-10-05 15:04:56 -0700

    C hello commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+  * main - A program that prints a link using puts
+  *
+  *Description: prints programming is like building a multilingual
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
